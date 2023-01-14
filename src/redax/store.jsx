945fdb5f333ed23b736a93daaae3ac47440753c3
GeorgiyDer <106014816+GeorgiyDer@users.noreply.github.com>
2023-01-14 12:57:24 +0200

    fin

--- src/redax/store.jsx
+++ src/redax/store.jsx
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit'
 import { filterSlice } from './contacts/filter.slice'
 import { contactSlice } from './contacts/contactsReduser'
 
-
 export const store = configureStore({
     reducer: {
         contacts: contactSlice.reducer,
